4fea778e752f98be9081b8b04a376be152901dd7
Timur Bykov <bikov.timur.013@gmail.com>
2022-01-15 16:02:11 +0300

    ADD: home

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ function Header() {
             </NavMenu>
             <UserImg src="/images/person.jpeg"/>
         </Nav>
-    )
+    );
 };
 
 export default Header;
@@ -64,7 +64,7 @@ const NavMenu = styled.div`
 
         img{
             height: 20px;
-        }
+        };
 
         span{
             font-size: 13px;
@@ -83,16 +83,16 @@ const NavMenu = styled.div`
                 transform-origin: left center;
                 transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
                 transform: scaleX(0);
-            }
-        }
+            };
+        };
 
         &:hover{
             span:after{
                 transform: scaleX(1);
                 opacity: 1;
-            }
-        }
-    }
+            };
+        };
+    };
 `;
 
 const UserImg = styled.img`
