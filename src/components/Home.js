dfb271b2f7830cb7c4d0c0a9a6d00f09effff60f
Timur Bykov <bikov.timur.013@gmail.com>
2022-01-18 12:00:51 +0300

    ADD: grab movies from DB

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,11 @@ import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
 import db from '../firebase';
+import {useDispatch} from "react-redux";
+import {setMovies} from "../features/movie/movieSlice";
 
 function Home() {
+    const dispatch = useDispatch();
 
     useEffect(()=>{
         db.collection("movies").onSnapshot((snapshot)=>{
@@ -13,6 +16,7 @@ function Home() {
                 return {id: doc.id, ...doc.data()}
             });
             console.log(tempMovies);
+            dispatch(setMovies(tempMovies));
         });
     }, []);
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,25 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-//   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-//   projectId: "disneyplus-clone-a33d5",
-//   storageBucket: "disneyplus-clone-a33d5.appspot.com",
-//   messagingSenderId: "37918794208",
-//   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-//   measurementId: "G-DRVLJKWRWG",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDEKOKoN6dgnjCS-QqVtLsQpEblm7My2y8",
-  authDomain: "disney-plus-clone-36b89.firebaseapp.com",
-  projectId: "disney-plus-clone-36b89",
-  storageBucket: "disney-plus-clone-36b89.appspot.com",
-  messagingSenderId: "973970359795",
-  appId: "1:973970359795:web:916d7a3d1052a39dd83e86",
-  measurementId: "G-ZJWZYTEY82"
+  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+  projectId: "disneyplus-clone-a33d5",
+  storageBucket: "disneyplus-clone-a33d5.appspot.com",
+  messagingSenderId: "37918794208",
+  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+  measurementId: "G-DRVLJKWRWG",
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDEKOKoN6dgnjCS-QqVtLsQpEblm7My2y8",
+//   authDomain: "disney-plus-clone-36b89.firebaseapp.com",
+//   projectId: "disney-plus-clone-36b89",
+//   storageBucket: "disney-plus-clone-36b89.appspot.com",
+//   messagingSenderId: "973970359795",
+//   appId: "1:973970359795:web:916d7a3d1052a39dd83e86",
+//   measurementId: "G-ZJWZYTEY82"
+// };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
