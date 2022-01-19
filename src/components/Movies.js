566fceb990e9711dd360727c11f8458123e12c4f
Timur Bykov <bikov.timur.013@gmail.com>
2022-01-19 11:48:11 +0300

    fire

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 import { selectMovies } from "../features/movie/movieSlice";
 import { useSelector } from "react-redux";
 
 function Movies() {
     const movies = useSelector(selectMovies);
-
-    console.log("This is movies", movies);
+    
 
 
     return (
         <Container>
             <h4>Recommended for You</h4>
             <Content>
-                {movies & 
-                movies.map((movie)=>{
-                    <Wrap key={movie.id}>
-                    <img src={movie.cardImg} alt="movieSimpsons" />
-                </Wrap>
-                })}
+                {movies &
+                    movies.map((movie) => {
+                        <Wrap key={movie.id}>
+                            <Link to={`/detail/${movie.id}`}>
+                                <img src={movie.cardImg} alt="movieSimpsons" />
+                            </Link>
+                        </Wrap>
+                    })}
             </Content>
         </Container>
     );
